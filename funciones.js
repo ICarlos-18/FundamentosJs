aaf3ce4535c1a744414946db9e04085673192299
ICarlos-s18 <csanchezhernandez567@gmail.com>
2023-09-12 12:15:09 -0600

    06 Funciones

---
+++ funciones.js
@@ -0,0 +1,65 @@
+// Funciones en Javascript
+/*
+function nombre(params){
+    body function
+}
+const nombre = function (params){
+    body funcrtion
+}
+*/
+// Funciones sin parametros, y sin retorno
+function saludo(){
+    console.log('Helloooo');
+}
+saludo();
+
+// Funciones con parametros sin retorno
+const saludaA = function(name){
+    console.log('Holaa ' + name);
+}
+saludaA('Clark Kent');
+
+// Funciones con parametros y retorno
+const fact = function (num) {
+    if(num === 0){
+        return 1
+    }
+    return fact(num -1) * num;
+}
+const result = fact(5);
+console.log(result);
+
+// Arrow function o funcionales de flecha
+// Sin parametros y sin retorno
+const saludaEnChino = () => {
+    console.log ('Ni Hao');
+}
+saludaEnChino();
+
+// con 1 parametro sin retorno
+const mayuscula = (word) => {
+    console.log(word.toUpperCase());
+}
+mayuscula('hola');
+
+const minuscula = (word) => {
+    console.log(word.toLowerCase());
+}
+minuscula('HELLO');
+
+// Dos o más parámetros y con retorno
+const fullName = (firstName, lastNmae) => {
+    return firstName + ' ' + lastNmae;
+}
+const nombreCompleto = fullName('Bruce', 'Wayne');
+console.log(nombreCompleto)
+
+//Si el return es la unica línea que se puede simplificar
+const duplica = num => num * 2;
+const doble = duplica(36);
+console.log(doble);
+
+/*function duplica(num){
+    return num * 2;
+}
+*/
